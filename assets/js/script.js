184a46e9f6f85f8f627db340e236380f27999053
Carol A. Nikolic <carolnikolic@gmail.com>
2022-12-08 01:02:08 -0300

    adding calculations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // Get input fields
 const bill = document.getElementById('bill');
 const numberPeople = document.getElementById('number-people');
+const inputTip = document.getElementById('custom');
+
+// Get radio buttons
+const radio = document.getElementsByTagName('input');
 
 // Get spans
 const warningBill = document.getElementById('warning-bill');
 const warningNumberPeople = document.getElementById('warning-number-people');
 
+// Get output fields
+const outputTip = document.getElementById('output-tip');
+const outputTotal = document.getElementById('output-total');
 
 // Verification input field
 // Add event listener for bill input field
@@ -18,7 +25,6 @@ numberPeople.addEventListener('keyup', () => {
     verifyPeople();
 });
 
-
 // Veridy input 0 for bill field
 const verifyBill = () => {
     if(bill.value == '0') {
@@ -30,7 +36,7 @@ const verifyBill = () => {
     }
 }
 
-// Veridy input 0 for number of people field
+// Verify input 0 for number of people field
 const verifyPeople = () => {
     if(numberPeople.value == '0') {
         numberPeople.style.border = '2px solid tomato';
@@ -41,29 +47,72 @@ const verifyPeople = () => {
     }
 }
 
+// Calculate tip based on default percentages
+function calculateTip() {
+    
+   
+    if(numberPeople.value > 0 || bill.value > 0) {
 
-// Calculate tip 
-function getRadioValue() {
-    let radio = document.getElementsByTagName('input');
-    for(i = 0; i < radio.length; i++) {
-        if(radio[i].checked) {
+        for(i = 0; i < radio.length; i++) {
+            if(radio[i].checked) {
 
-            let totalTip = bill.value * radio[i].value / 100;
-            let tipPerPerson = totalTip / numberPeople.value;
+                let totalTip = bill.value * radio[i].value / 100;
+                let tipPerPerson = totalTip / numberPeople.value;
 
-            let billPerPerson = bill.value / numberPeople.value;
-            let totalPerPerson = billPerPerson + tipPerPerson;
-            
-            alert(totalPerPerson);
-            
-        }   
+                let billPerPerson = bill.value / numberPeople.value;
+                let totalPerPerson = billPerPerson + tipPerPerson;
+                
+                // alert(totalPerPerson);
+                
+                outputTip.innerHTML =  '$' + Math.round(tipPerPerson);
+                outputTotal.innerHTML = '$' + Math.round(totalPerPerson);
+                
+            }  
+        }
+    }
+}
+   
+// Calculate tip with custom tip percentage 
+function calculateCustomTip() {   
+
+    // Remove special characters from imput
+    let customTip = inputTip.value;
+    let customTipNumber = customTip.replace(/[^\w ]/g, '');
+    console.log(customTipNumber);
+
+    // Check if custom input is bigger then zero to initiate calculation
+    if(customTipNumber > 0) { 
+
+        //Calculate tip
+        let totalTip = bill.value * customTipNumber / 100; //10
+        let tipPerPerson = totalTip / numberPeople.value; //5
+
+        // Calculate bill and tip per person
+        let billPerPerson = bill.value / numberPeople.value; //
+        let totalPerPerson = billPerPerson + tipPerPerson;
+        
+        // Add result to output on layout
+        outputTip.innerHTML =  '$' + Math.round(tipPerPerson);
+        outputTotal.innerHTML = '$' + Math.round(totalPerPerson);       
+    } 
+}
+        
+// Change output infinity and NaN
+function removeInfinityNan() {
+    if(outputTip.value == '$Infinity' || outputTip.value == '$NaN') {
+        outputTip.innerHTML =  '$';
+        outputTotal.innerHTML = '$';
     }
 }
 
 // Event listener for last input (number of people) being field
-numberPeople.addEventListener('blur', () => {
+numberPeople.addEventListener('keyup', () => {
 
-    getRadioValue();
- 
+    calculateTip();
+   
+    calculateCustomTip();
+
+    removeInfinityNan();
+
 });
 
