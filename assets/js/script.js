b1207ef7723b63cfba7e130db35c58ea4390c9b0
Carol A. Nikolic <carolnikolic@gmail.com>
2022-12-09 18:47:06 -0300

    html: changing input radio to button, css: adding style fo focus state and spans; js: adding automatic calculations, reset functionality, changes for infinity and nan

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,31 @@ const bill = document.getElementById('bill');
 const numberPeople = document.getElementById('number-people');
 const inputTip = document.getElementById('custom');
 
-// Get radio buttons
-const radio = document.getElementsByTagName('input');
-
 // Get spans
 const warningBill = document.getElementById('warning-bill');
+const warningCustomTip = document.getElementById('warning-custom');
 const warningNumberPeople = document.getElementById('warning-number-people');
+
+// Get default tip percentage button and save it's value in span element
+const tipButtons = document.querySelectorAll('.bt-tip');
+const saveValue = document.getElementById('tip');
 
 // Get output fields
 const outputTip = document.getElementById('output-tip');
 const outputTotal = document.getElementById('output-total');
 
-// Verification input field
-// Add event listener for bill input field
-bill.addEventListener('keyup', () => {
-   verifyBill();
-});
+// Get reset button
+const reset = document.getElementById('reset-bt');
+
+
 
 // Add event listener for people input field
 numberPeople.addEventListener('keyup', () => {
     verifyPeople();
 });
 
-// Veridy input 0 for bill field
+
+// Verify input 0 for bill field
 const verifyBill = () => {
     if(bill.value == '0') {
         bill.style.border = '2px solid tomato';
@@ -33,6 +35,22 @@ const verifyBill = () => {
     } else {
         bill.style.border = '';
         warningBill.textContent = '';
+    }
+}
+
+// Verify input 0 for custom tip field
+const verifyCustomTip = () => {
+    if(inputTip.value == '0') {
+        inputTip.style.border = '2px solid tomato';
+        warningCustomTip.textContent = "Can't be zero";
+        inputTip.style.backgroundColor = 'white';
+        inputTip.style.color = 'hsl(183, 100%, 15%)';
+
+    } else {
+        inputTip.style.border = '';
+        warningCustomTip.textContent = '';
+        inputTip.style.backgroundColor = '';
+        inputTip.style.color = '';
     }
 }
 
@@ -46,42 +64,17 @@ const verifyPeople = () => {
         warningNumberPeople.textContent = '';
     }
 }
-
-// Calculate tip based on default percentages
-function calculateTip() {
-    
-   
-    if(numberPeople.value > 0 || bill.value > 0) {
-
-        for(i = 0; i < radio.length; i++) {
-            if(radio[i].checked) {
-
-                let totalTip = bill.value * radio[i].value / 100;
-                let tipPerPerson = totalTip / numberPeople.value;
-
-                let billPerPerson = bill.value / numberPeople.value;
-                let totalPerPerson = billPerPerson + tipPerPerson;
-                
-                // alert(totalPerPerson);
-                
-                outputTip.innerHTML =  '$' + Math.round(tipPerPerson);
-                outputTotal.innerHTML = '$' + Math.round(totalPerPerson);
-                
-            }  
-        }
-    }
-}
    
 // Calculate tip with custom tip percentage 
 function calculateCustomTip() {   
-
+   
     // Remove special characters from imput
     let customTip = inputTip.value;
     let customTipNumber = customTip.replace(/[^\w ]/g, '');
-    console.log(customTipNumber);
+
 
     // Check if custom input is bigger then zero to initiate calculation
-    if(customTipNumber > 0) { 
+    if(customTipNumber > 0 && numberPeople.value > 0 && bill.value > 0) { 
 
         //Calculate tip
         let totalTip = bill.value * customTipNumber / 100; //10
@@ -93,26 +86,164 @@ function calculateCustomTip() {
         
         // Add result to output on layout
         outputTip.innerHTML =  '$' + Math.round(tipPerPerson);
-        outputTotal.innerHTML = '$' + Math.round(totalPerPerson);       
+        outputTotal.innerHTML = '$' + Math.round(totalPerPerson); 
+           
+    } else {
+        outputTip.innerHTML =  '$' ;
+        outputTotal.innerHTML = '$'; 
     } 
 }
         
-// Change output infinity and NaN
+// Remove possible Infinity and NaN from layout output
 function removeInfinityNan() {
-    if(outputTip.value == '$Infinity' || outputTip.value == '$NaN') {
-        outputTip.innerHTML =  '$';
-        outputTotal.innerHTML = '$';
-    }
+    outputTip.innerHTML =  '$';
+    outputTotal.innerHTML = '$';
 }
 
-// Event listener for last input (number of people) being field
-numberPeople.addEventListener('keyup', () => {
+function calculateTip() {
+    // Select buttons with class tip. Iterate through the buttons to see if there was a click event
+    tipButtons.forEach((tip) => {
+        tip.addEventListener("click", () => {
+            
+            //Reset custom field if clicked in default percentaged after inputing custom percentage
+            if(inputTip.value > 0) {
+                inputTip.value = '';
+                inputTip.value = inputTip.valueinnerHTML = '';
+            }
+            
+            // Fix NaN
+            bill.value = bill.value || '';
+            tip.value = tip.value || '';
+            numberPeople.value = numberPeople.value || '';
 
+            // Select the element to save value
+            tipButtons.innerHTML = tip.innerHTML;
+
+            // Remove infinity and NaN result
+            if(outputTip.value == '$Infinity' || outputTip.value == '$NaN') {
+                removeInfinityNan();
+            } 
+            
+            // Prevents infinity result
+            if(numberPeople.value > 0 && bill.value > 0) {
+            // Calculate tip and total
+            let totalTip = bill.value * tip.value / 100;
+            let tipPerPerson = totalTip / numberPeople.value;
+            let billPerPerson = bill.value / numberPeople.value;
+            let totalPerPerson = billPerPerson + tipPerPerson;
+            
+            // Round tip and bill result
+            outputTip.innerHTML =  '$' + Math.round(tipPerPerson * 10) / 10;
+            outputTotal.innerHTML = '$' + Math.round(totalPerPerson * 10) / 10;
+            }
+        });
+
+        bill.addEventListener('keyup', () => {
+
+            // Select the element to save value
+            tipButtons.innerHTML = tip.innerHTML;
+
+            // Remove infinity and NaN result
+            if(outputTip.value == '$Infinity' || outputTip.value == '$NaN') {
+                removeInfinityNan();
+            } 
+            
+            // Prevents infinity result
+            if(numberPeople.value > 0 && bill.value > 0) {
+                
+                // Calculate tip and total
+                let totalTip = bill.value * tip.value / 100;
+                let tipPerPerson = totalTip / numberPeople.value;
+                let billPerPerson = bill.value / numberPeople.value;
+                let totalPerPerson = billPerPerson + tipPerPerson;
+                
+                // Round tip and bill result
+                outputTip.innerHTML =  '$' + Math.round(tipPerPerson * 10) / 10;
+                outputTotal.innerHTML = '$' + Math.round(totalPerPerson * 10) / 10;
+            }
+        });
+
+        numberPeople.addEventListener('keyup', () => {
+
+            // Select the element to save value
+            tipButtons.innerHTML = tip.innerHTML;
+
+            // Remove infinity and NaN result
+            if(outputTip.value == '$Infinity' || outputTip.value == '$NaN') {
+                removeInfinityNan();
+            } 
+            
+            // Prevents infinity result
+            if(numberPeople.value > 0 && bill.value > 0) {
+                
+                // Calculate tip and total
+                let totalTip = bill.value * tip.value / 100;
+                let tipPerPerson = totalTip / numberPeople.value;
+                let billPerPerson = bill.value / numberPeople.value;
+                let totalPerPerson = billPerPerson + tipPerPerson;
+                
+                // Round tip and bill result
+                outputTip.innerHTML =  '$' + Math.round(tipPerPerson * 10) / 10;
+                outputTotal.innerHTML = '$' + Math.round(totalPerPerson * 10) / 10;
+            }
+            console.log('test');
+        });
+
+        inputTip.addEventListener('keyup', () => {
+            calculateCustomTip();
+        });
+    });
+}
+
+
+// Event listener for input bill being field
+bill.addEventListener('keyup', () => {
+
+    // Verify bill input field
+    verifyBill();
+
+    // Calculate with custom percentage values
     calculateTip();
    
+    // Calculate with default tip percetage values
     calculateCustomTip();
 
-    removeInfinityNan();
+});
+
+// Event listener for input custom tip being field
+inputTip.addEventListener('keyup', () => {
+
+    // Verify custom tip input field
+    verifyCustomTip();
+
+    // Calculate with default tip percetage values
+    calculateTip();
+   
+    // Calculate with custom percentage values
+    calculateCustomTip();
+
+});
+
+// Event listener for input number of people being field
+numberPeople.addEventListener('keyup', () => {
+
+    // Verify number of people input field
+    verifyPeople();
+
+    // Calculate with default tip percetage values
+    calculateTip();
+   
+    // Calculate with custom percentage values
+    calculateCustomTip();
+
+});
+
+// Event listener for button reset - reseting result in output
+
+reset.addEventListener('click', () => {
+
+    outputTip.innerHTML =  '$';
+    outputTotal.innerHTML = '$';
 
 });
 
